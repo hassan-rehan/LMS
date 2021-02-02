53a37a693d389bb9ca3968539a4f30f71dd26119
Hassan_Rehan <jutthassan30@gmail.com>
2021-02-02 23:23:19 +0500

    Adding static urls, media and templates directories.

--- LMS/urls.py
+++ LMS/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
